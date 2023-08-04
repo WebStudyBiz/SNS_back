267e66137d3469e4d93cf88661e984a558864807
jieun <jieun7187@gachon.ac.kr>
2023-08-04 20:56:38 +0900

    first commit

---
+++ src/main/java/com/example/snsproject_biz/SnsProjectBizApplication.java
@@ -0,0 +1,13 @@
+package com.example.snsproject_biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsProjectBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsProjectBizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/snsproject_biz/SnsProjectBizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snsproject_biz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsProjectBizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
