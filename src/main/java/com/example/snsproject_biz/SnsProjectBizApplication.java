adcd960f030659dba370526bec41abb3bcd8c93b
jieun <110973350+jieun0407@users.noreply.github.com>
2023-08-20 21:27:06 +0900

    test branch1

--- src/main/java/com/example/snsproject_biz/SnsProjectBizApplication.java
+++ src/main/java/com/example/snsproject_biz/SnsProjectBizApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SnsProjectBizApplication {
 
     public static void main(String[] args) {
+        System.out.println("jieun");
         SpringApplication.run(SnsProjectBizApplication.class, args);
     }
 
